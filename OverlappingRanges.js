9d5f039c3b3ae2235f5ee39f89204dbfe9c917c0
Markus <markus.sanderst@gmail.com>
2019-03-04 15:36:13 -0600

    Prompt

---
+++ OverlappingRanges.js
@@ -0,0 +1,7 @@
+// Determine overlapping numbers in ranges.
+
+//You will be given an array with 5 numbers. The first 2 numbers represent a range, and the next two numbers represent another range. 
+//The final number in the array is X. The goal of your program is to determine if both ranges overlap by at least X numbers. 
+//For example, in the array [4, 10, 2, 6, 3] the ranges 4 to 10 and 2 to 6 overlap by at least 3 numbers (4, 5, 6),
+// so your program should return true.
+
