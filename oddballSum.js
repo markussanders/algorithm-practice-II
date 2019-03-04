c80bc0a8d4cd3a5cc26e6c25ca64a9c99455f7fe
Markus <markus.sanderst@gmail.com>
2019-03-04 15:29:31 -0600

    Solved

--- oddballSum.js
+++ oddballSum.js
@@ -1,1 +1,6 @@
 // Write a function called oddball_sum which takes in a list of numbers and returns the sum of all the odd elements.
+
+function oddballSum(nums) {
+    return nums.filter(num => num % 2 > 0)
+    .reduce((a, b) => (a + b), 0);
+}
